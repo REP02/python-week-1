2e634876e84bccdefa745bcdd3e56a84cbc1c6c3
REP02 <ifymarshal217@gmail.com>
2025-08-19 17:03:41 +0100

    week 1 assignment

---
+++ lesson_1.py
@@ -0,0 +1,28 @@
+# This is a simple python script that demonstrates an object counter
+'''
+str_counter = 0
+
+#or item in ("apple", 16, "cherry", "date", "elderberry", 42, "fig", 3.14):
+  #  if isinstance(item, str):
+   #  str_counter += 1
+
+print(str_counter)
+'''
+
+#This is a simple python script that calculates the sum of a list of numbers
+'''
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+for number in numbers:
+    total += number
+print(total)
+'''
+# This is a simple python script that calculates the average or mean of a list of numbers
+'''
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+for number in numbers:
+    total += number
+average = total / len(numbers)
+print(average)
+'''
