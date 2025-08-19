a35c5d8d78e30b87631b3e5d71629fa0d4f191a9
REP02 <ifymarshal217@gmail.com>
2025-08-19 16:56:32 +0100

    add file for week 1 assignment

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# create a program that asks the user to input two numbers and a mathematical operation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operation")
